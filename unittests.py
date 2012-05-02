92891fe453895a320051cc3abe361003ccf0850b
David Martinez <dmartinez@touchtype-online.com>
2012-05-02 14:39:42 +0200

    Adding order 10 tests, testGenList and 6D

--- unittests.py
+++ unittests.py
@@ -22,7 +22,7 @@ class TestExtended:
 
   fxyzd = ['fx', 'fpx', 'fy', 'fpy']
 
-  sf = "%.10e"
+  sf = "%.8e"
 
   compare = False
   correlation = False
@@ -72,6 +72,21 @@ class TestExtended:
     if self.compare:
       self.assertAlmostEq(self.m.compc(self.m2,v=self.fxyzd), self.mm.compc(self.mm2))
 
+  def testGenList(self):
+    for v in self.fxyzd:
+      self.mm.generatelist(v[1:], self.sigmaFFS)
+      for (l1,l2) in zip(self.m.generatelist(v, self.sigmaFFS, self.gaussian), getattr(self.mm, 'list' + v[1:])):
+        self.assertAlmostEq(l1[0],l2[0])
+        # Because the order in which the indices are stored isn't the
+        # same the result isn't equal but equivalent.
+        # E.g.
+        # l1[2:] = (1,2,3,0,1,0) equal l2[2:] = (1,2,3,0,1,0)
+        # l1[2:] = (1,2,3,0,1,0) equivalent l2[2:] = (0,1,0,1,2,3)
+        ind1 = l1[2:]
+        ind2 = l2[2:]
+        l = len(ind1)/2
+        self.assertTrue((ind1[:l] == ind2[:l] or ind1[:l] == ind2[l:]) and (ind1[l:] == ind2[:l] or ind1[l:] == ind2[l:]))
+
 ################
 ## Test cases ##
 ################
@@ -85,7 +100,7 @@ class Test5var6order(unittest.TestCase, TestExtended):
     f = 'assets/fort.18'
     ff = 'assets/5fort.18'
     self.compare = True
-    self.vals = OrderedDict([('x',1E-19),('px',3E-23),('y',5E-32),('py',6E-12),('d',-1.5E23),('s',0)])
+    self.vals = OrderedDict([('x',self.sigmaFFS[0]),('px',self.sigmaFFS[1]),('y',self.sigmaFFS[2]),('py',self.sigmaFFS[3]),('d',self.sigmaFFS[4]),('s',0)])
 
     self.m = Map2(order=o,filename=f)
     self.mm = Map(order=o,filename=f)
@@ -101,11 +116,60 @@ class Test5var6orderGaussian(unittest.TestCase, TestExtended):
     f = 'assets/fort.18'
     self.gaussian = True
     self.correlation = True
-    self.vals = OrderedDict([('x',1E-19),('px',3E-23),('y',5E-32),('py',6E-12),('d',-1.5E23),('s',0)])
+    self.vals = OrderedDict([('x',self.sigmaFFS[0]),('px',self.sigmaFFS[1]),('y',self.sigmaFFS[2]),('py',self.sigmaFFS[3]),('d',self.sigmaFFS[4]),('s',0)])
 
     self.m = Map2(order=o,filename=f)
     self.mm = Map(order=o,filename=f)
 
+class Test5var10order(unittest.TestCase, TestExtended):
+
+  def setUp(self):
+    from mapclass25 import Map
+
+    o = 10
+    f = 'assets/fort.18'
+    ff = 'assets/5fort.18'
+    self.compare = True
+    self.vals = OrderedDict([('x',self.sigmaFFS[0]),('px',self.sigmaFFS[1]),('y',self.sigmaFFS[2]),('py',self.sigmaFFS[3]),('d',self.sigmaFFS[4]),('s',0)])
+
+    self.m = Map2(order=o,filename=f)
+    self.mm = Map(order=o,filename=f)
+    self.m2 = Map2(order=o,filename=ff)
+    self.mm2 = Map(order=o,filename=ff)
+
+class Test5var10orderGaussian(unittest.TestCase, TestExtended):
+
+  def setUp(self):
+    from mapclassGaussianDelta25 import Map
+
+    o = 10
+    f = 'assets/fort.18'
+    self.gaussian = True
+    self.correlation = True
+    self.vals = OrderedDict([('x',self.sigmaFFS[0]),('px',self.sigmaFFS[1]),('y',self.sigmaFFS[2]),('py',self.sigmaFFS[3]),('d',self.sigmaFFS[4]),('s',0)])
+
+    self.m = Map2(order=o,filename=f)
+    self.mm = Map(order=o,filename=f)
+
+class Test6var6order(unittest.TestCase, TestExtended):
+
+  def setUp(self):
+    from mapclass25_6var import Map
+
+    self.sigmaFFS = [0.000227,9.306e-5, 5.02e-4, 4.21e-5,0.00666,0.002]
+
+    o = 6
+    f = 'assets/6Dfort.18'
+    self.vals = OrderedDict([('x',self.sigmaFFS[0]),('px',self.sigmaFFS[1]),('y',self.sigmaFFS[2]),('py',self.sigmaFFS[3]),('d',self.sigmaFFS[4]),('s',self.sigmaFFS[5])])
+
+    self.m = Map2(order=o,filename=f)
+    self.mm = Map(order=o,filename=f)
+
+  @unittest.skip("Unnecessary")
+  def testGenList(self):
+    # generatelist isn't clear in mapclass25_6var so override this
+    # test to avoid running it
+    return
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Tester for MapClass')
